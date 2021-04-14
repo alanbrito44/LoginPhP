e4d6565bc55713fccac5a74e9f60d6ca31c6c075
javAmay1 <xxcastillo_199xx@hotmal.com>
2021-04-14 14:31:29 -0600

    fondo agregado a login

--- login.php
+++ login.php
@@ -38,8 +38,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Pagina Principal</title>
 </head>
-<body>
-
+<body style="background-image: url('img/fondo.jpg');">
     <div class="container">
         <h1>Login de usuario</h1>
 
